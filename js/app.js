052c92c992278f7e1334168814ba490d399f9d22
Maria Agostina Albornoz <Agostinaalbornoz43@gmail.com>
2022-03-23 11:12:48 -0300

    Variables y operadores en javascrip

---
+++ js/app.js
@@ -0,0 +1,75 @@
+
+//codigo javascrip
+alert("Bienvenidos a javascrip!!"); //mensaje de alerta (ventana nueva)
+console.log("Bienvenidos a javascrip!!"); //muestra por consola
+console.log(5 + 3);
+
+//mostrar en el navegador
+document.write("Es un gusto saludarte en este curso");
+
+
+//variables
+var nombre;
+nombre = 'pepito';
+
+let apellido = 'Gonzalez';
+
+const pi = 3.14;
+
+//tipos de datos
+
+//string 
+let cliente = 'Fabricio'
+let alumno='Roberto Gonzalez'
+let mensaje='Hola mundo, soy feliz'
+
+//number
+let numero1=34
+let numero2=52
+let numero3=56.89
+
+//boolean
+let comprar=true
+let comer=false
+
+//null
+let billetera=null
+
+//undefined
+let caja
+
+//array o arreglos
+let arreglo = ['Pablo', 45, false]
+let alumnos = ['Jose', 'Alberto', 'Florida']
+
+//object u objetos
+let persona = {
+    nombre:'Josefina',
+    apellido:'Mandarino',
+    edad:'33'
+}
+
+//operadores
+let num1=20;
+let num2=2;
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+
+let resto = num1 % num2; //devuelve el resto de la division
+
+//unitarios
+++num2
+--num2
+
+//relacionales 
+num1 = 22;
+num2 = "22";
+console.log(num1 === num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+console.log(num1 != num2);
+
+console.log("El resultado de la suma es" + suma);
