99f3a25a7cb8a465fc84c2a7ae179c33d4716663
Maria Agostina Albornoz <Agostinaalbornoz43@gmail.com>
2022-03-23 11:29:36 -0300

    Agrego una nueva carpeta llamada Tareas

---
+++ js/Tareas/23-03-2022.js
@@ -0,0 +1,8 @@
+//Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga “un mensaje”.
+alert("un mensaje"); 
+
+//Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello World» (document.write).
+document.write("Hello Word");
+
+//Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+console.log(5 + 3);
