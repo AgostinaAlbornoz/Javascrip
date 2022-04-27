8f6a2875f465b90cb2640611a2cf3bbda98f62c7
Maria Agostina Albornoz <Agostinaalbornoz43@gmail.com>
2022-04-27 15:18:35 -0300

    Agrego la tarea y lo visto en la ultima clase del 25-04

--- js/app.js
+++ js/app.js
@@ -1,4 +1,33 @@
+let data = [
+    {
+      titulo:"Animales fantásticos",
+      genero: " Aventura/Fantasía",
+      año: "2022",
+      sinopsis: "",
+    },
+    {
+        titulo:"Sonic 2",
+        genero: "Aventura/Fantasía ",
+        año: "2022",
+        sinopsis: " ",
+    },
+    {
+        titulo:"La ciudad perdida",
+        genero: "Comedia/Accion",
+        año: "2022",
+        sinopsis: "  ",
+    },
+   
+  ];
 
+  console.log("--- Peliculas ---")
+  for (let i = 0; i < data.length; i++) {
+    console.log(`Titulo: ${data[i].titulo}`);
+    console.log(`Genero: ${data[i].genero}`);
+    console.log(`Año: ${data[i].año}`);
+    console.log(`Sinopsis: ${data[i].sinopsis}`);
+    console.log(`--------------------`);
+  }
 
 
 
